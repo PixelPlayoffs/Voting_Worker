3381d63242348ef0bda61eebb874e82ffb1eeffb
tripdubroot <ian.philpot@microsoft.com>
2017-04-05 11:04:39 -0400

    Added comments to config

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,6 @@
 var config = {}
 
+// This HAS to be set as env in contaner
 config.host = process.env.AZURE_SERVICEBUS_NAMESPACE || "AZURE_SERVICEBUS_NAMESPACE";
 config.authKey = process.env.AZURE_SERVICEBUS_ACCESS_KEY || "AZURE_SERVICEBUS_ACCESS_KEY";
 
